62fee0c2bc551ac88e68fdd1201f6552f2de87b1
Ege Meriç Erdoğan <egemeric99@gmail.com>
2021-02-20 11:47:47 +0300

    added language guess TR and EN

--- textanalyzer.py
+++ textanalyzer.py
@@ -1,9 +1,10 @@
 import re
+
+
 class TextAnalyzer:
 
     def __init__(self, text):
         self.send_json_obj = {}
-        print("textttt", text)
         self.words = re.findall(r"\w+", text)
         self.__get_word_dict()  # union the all words (no occurrence)
         self.word_count_dict = self.__find_texts_word_ct(self.word_dict)  # find all words's length
@@ -69,11 +70,21 @@ class TextAnalyzer:
         word_count = self.__create_zero_word_dict_values(self.words)
         word_count = self.__find_texts_word_ct(self.words)
         word_count = self.__sort_text_dict_values(word_count)
-        self.med_word = word_count[int(len(word_count)/2)][0]
+        self.med_word = word_count[int(len(word_count) / 2)][0]
         return self.med_word
 
     def language(self):
+        self.__language()
+        self.send_json_obj.update({"language": self.lng})
         pass
+
+    def __language(self):
+        guess_obj = LanguageGuess(self.words,
+                                  len(self.word_dict))  # send  initial words and total words count without repetition
+        if guess_obj.score > 10:
+            self.lng = "EN"
+        else:
+            self.lng = "TR"
 
     def topfivewords(self):
         word_occurrence = self.__create_zero_word_dict_values(self.word_dict)
@@ -97,10 +108,58 @@ class TextAnalyzer:
         return text_word_dict
 
     def __sort_text_dict_values(self, a_text_dict):
-        return sorted(a_text_dict.items(), key=lambda a: a[1], reverse= True)
+        return sorted(a_text_dict.items(), key=lambda a: a[1], reverse=True)
 
-    def __find_texts_word_ct(self,a_dict):
+    def __find_texts_word_ct(self, a_dict):
         length_by_word = self.__create_zero_word_dict_values(a_dict)
         for i in self.words:
             length_by_word.update({i: len(i)})
         return length_by_word
+
+
+start_eng_keywords = {"am", "is", "are", "did", "I", "am", "we", "they", "them",
+                      "It", "on", "in", "the", "to", "he", "she", "my", "her", "his",
+                      "our", "us", "what", "why", "when", "much", "many"
+                      }
+
+
+class LanguageGuess:
+    global start_eng_keywords
+    learned_eng_keywords = set()
+    score_table = {}
+
+    def __init__(self, words, total_words):
+        self.start_eng_keywords = start_eng_keywords
+        self.__words_initial = words  # initial words
+        self.__total_words_initial = total_words  # total words without repetition
+        self.__create_score_table()  # create initial score table
+        self.__guess_eng_lang()  # match words with start_eng_words
+        self.__calc_score()  # calc occurrence
+        self.__update_learned_eng_words()  # hypothetically all entered text is grammatically true
+
+    def __create_score_table(self):
+        print(self.start_eng_keywords)
+        for i in self.start_eng_keywords:
+            self.score_table.update({i: 0})
+
+    def __guess_eng_lang(self):
+        for i in self.__words_initial:
+            score = self.score_table.get(i)
+            if score is not None:
+                self.score_table.update({i: score + 1})
+
+    def __update_learned_eng_words(self):
+        global start_eng_keywords
+        if self.score > 10:  # if score is gr than 10 text is eng
+            for i in self.__words_initial:
+                self.learned_eng_keywords.add(i)
+            start_eng_keywords = self.start_eng_keywords.union(self.learned_eng_keywords)
+            print("learned eng keywords len:", len(self.learned_eng_keywords))
+        print("Eng Score", self.score)
+
+    def __calc_score(self):
+        score = 0
+        for i in self.score_table.values():
+            score += i
+        self.score = score / self.__total_words_initial * 100
+        return self.score
